c25853d4ee9bbd8c33a276b735478b948b6054d6
NaNdalal-dev <56131112+NaNdalal-dev@users.noreply.github.com>
2019-10-08 19:54:33 +0530

    updated to perfect numbers

--- number.py
+++ number.py
@@ -187,3 +187,34 @@ def i_amst(num1,num2):
             n = n // 10
         if (i == ams):
             print(i)
+
+def perfect(n):
+    l = n
+    m = 0
+    for i in range(1, n):
+        if (n % i == 0):
+            m = m + i
+    if (m == l):
+        return  True
+    else:
+        return False
+
+def n_perfect(l):
+    for n in range(1, l + 1):
+        m = 0
+        for i in range(1, n):
+            if (n % i == 0):
+                m = m + i
+        if (n == m):
+            print(m)
+
+
+def i_perfect(n1,n2):
+    for n in range(n1, n2 + 1):
+        m = 0
+        for i in range(1, n):
+            if (n % i == 0):
+                m = m + i
+        if (n == m):
+            print(m)
+            
